5916024036a58abd40f23d871eab7329f274ea6d
Si Thu Hein <sithuhein26@gmail.com>
2022-11-03 00:02:33 +0630

    sithudev

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
+
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap"
-
+import swal from 'sweetalert2'
+window.Swal = swal
+window.$=window.jquery=require('jquery')
 createApp(App).use(store).use(router).mount('#app')
